c1c7be29b4dda0c9faa751697393013936c5ca0c
Mohamed Mousa <mohamed312mousa@gmail.com>
2025-05-11 19:33:16 +0300

    initializing the Routes

---
+++ src/Root/Layout/Routes.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Login from '../../Pages/Login/Login';
+import Layout from './Layout';
+import Dashboard from '../../Pages/Dashboard/Dashboard';
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Login" element={<Login />} />
+        <Route element={<Layout />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoutes;
